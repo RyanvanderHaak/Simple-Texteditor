5ecca93bf5136eafc78e389bd566af6392f2a1f9
Ryan van der Haak <159620802+RyanvanderHaak@users.noreply.github.com>
2024-07-06 10:45:41 +0200

    Simple Texteditor Is Here!!
    Hello everyone!
    
    I'm excited to tell about my new online, simple texteditor for writing code in HTML, CSS and JavaScript! You don't have to download anything, everything is online! You can choose between 4 themes to customize your editor. Go to https://ryanvanderhaak.github.io/Simple-Texteditor/index.html to check out the editor.
    
    I'll make more themes soon and happy coding!
    Ryan

---
+++ script.js
@@ -0,0 +1,55 @@
+
+    const htmlEditor = CodeMirror.fromTextArea(document.getElementById('html-editor'), {
+      mode: 'xml',
+      lineNumbers: true,
+      theme: 'nature',
+      extraKeys: {"Ctrl-Space": "autocomplete"},
+      hintOptions: {schemaInfo: CodeMirror.htmlSchema}
+  });
+
+  const cssEditor = CodeMirror.fromTextArea(document.getElementById('css-editor'), {
+      mode: 'css',
+      lineNumbers: true,
+      theme: 'nature',
+      extraKeys: {"Ctrl-Space": "autocomplete"}
+  });
+
+  const jsEditor = CodeMirror.fromTextArea(document.getElementById('js-editor'), {
+      mode: 'javascript',
+      lineNumbers: true,
+      theme: 'nature',
+      extraKeys: {"Ctrl-Space": "autocomplete"}
+  });
+
+
+  document.getElementById('run-btn').addEventListener('click', () => {
+      const htmlContent = htmlEditor.getValue();
+      const cssContent = `<style>${cssEditor.getValue()}</style>`;
+      const jsContent = `<script>${jsEditor.getValue()}<\/script>`;
+      const output = document.getElementById('output');
+
+      output.contentDocument.body.innerHTML = htmlContent + cssContent + jsContent;
+  });
+
+  document.getElementById('theme-selector').addEventListener('change', (e) => {
+      const theme = e.target.value;
+      htmlEditor.setOption('theme', theme);
+      cssEditor.setOption('theme', theme);
+      jsEditor.setOption('theme', theme);
+  });
+
+  document.getElementById('export-btn').addEventListener('click', () => {
+      const htmlContent = htmlEditor.getValue();
+      const cssContent = cssEditor.getValue();
+      const jsContent = jsEditor.getValue();
+
+      const zip = new JSZip();
+      zip.file("index.html", htmlContent);
+      zip.file("styles.css", cssContent);
+      zip.file("scripts.js", jsContent);
+
+      zip.generateAsync({ type: "blob" })
+          .then(function(content) {
+              saveAs(content, "MySimpleTextEditorProject.zip");
+          });
+  });
